da40536a8e8f8d19f08440eb01b6970fdfebe0eb
dimashpk <shupimonstr@gmail.com>
2020-06-06 13:50:34 +0300

    Updates

---
+++ precache-manifest.4007651a05497d8577a71fec095c2552.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a2f29fb93b95d784db6a12ba87d856",
+    "url": "/itCamasutra/index.html"
+  },
+  {
+    "revision": "9196d118f22637f3c1d6",
+    "url": "/itCamasutra/static/css/3.bf6e0f5a.chunk.css"
+  },
+  {
+    "revision": "8bcca11f478628e07bfa",
+    "url": "/itCamasutra/static/css/4.ec0c55f7.chunk.css"
+  },
+  {
+    "revision": "904e035a757565618eba",
+    "url": "/itCamasutra/static/css/5.0fc1a1b8.chunk.css"
+  },
+  {
+    "revision": "95547d994f02e64e9114",
+    "url": "/itCamasutra/static/css/main.3f09c7fc.chunk.css"
+  },
+  {
+    "revision": "6e210911e16e0947727e",
+    "url": "/itCamasutra/static/js/2.f679427b.chunk.js"
+  },
+  {
+    "revision": "9196d118f22637f3c1d6",
+    "url": "/itCamasutra/static/js/3.532e2a12.chunk.js"
+  },
+  {
+    "revision": "8bcca11f478628e07bfa",
+    "url": "/itCamasutra/static/js/4.9a3001a5.chunk.js"
+  },
+  {
+    "revision": "904e035a757565618eba",
+    "url": "/itCamasutra/static/js/5.df432cc0.chunk.js"
+  },
+  {
+    "revision": "95547d994f02e64e9114",
+    "url": "/itCamasutra/static/js/main.a408690c.chunk.js"
+  },
+  {
+    "revision": "55cdaba36414a31c9552",
+    "url": "/itCamasutra/static/js/runtime~main.1d14d76f.js"
+  },
+  {
+    "revision": "d9870e6d32750d8fb3f2672693b084b0",
+    "url": "/itCamasutra/static/media/user.d9870e6d.png"
+  }
+]);
